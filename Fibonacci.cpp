5fb4eb94b9305f64d8d738875066e008de6e8cc8
Jorge1398 <54086172+Jorge1398@users.noreply.github.com>
2019-10-07 12:45:42 -0500

    Serie Fibonacci (recursivo)
    EL sistema aun no hace su función, falta revisión.

---
+++ Fibonacci.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#include<iostream>
+
+void fibonaci(int x);
+
+int main()
+{
+	int x;
+	cout<<"Ingrese la cantidad de terminos: ";
+	cin>>x;
+	fibonaci(x);
+	return 0;
+}
+
+void fibonaci(int x)
+{
+	if(int x==1)
+	   return 0;
+	else
+	(if==2)
+	   return==1;
+	else
+	   return fibonaci(x-1)+(x-2);
+}
